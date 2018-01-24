e8bc5a1025ab4ee603e8d938e85a56abbef68072
james_hargreaves <jh2045@cam.ac.uk>
2018-01-24 14:23:48 +0000

    with docker implementation set up

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 8080,
   mongoose = require('mongoose'),
   Model = require('./api/models/newGreyDbModel'), //created model loading here
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  config = require('./config');
+
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/new_grey_db'); 
-
+mongoose.connect(config.db[app.settings.env]);  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // * => allow all origins
@@ -20,7 +20,6 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Accept'); // add remove headers according to your needs
   next()
 })
-
 
 var routes = require('./api/routes/newGreyDbRoutes'); //importing route
 routes(app); //register the route
