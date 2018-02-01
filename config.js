d8a7f69d36389186a9b914715d8c52d0440ee0ee
james_hargreaves <jh2045@cam.ac.uk>
2018-02-01 18:15:27 +0000

    with more correct location

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var location = "35.177.160.3"
 module.exports = {  
   db: {
       production: "mongodb://"+location+":27017/test -u testNext -p pass",
-      development: "mongodb://"+location+":27017/  -u testNext -p pass",
+      development: "mongodb://"+location+":27017/test  -u testNext -p pass",
   }
 };
