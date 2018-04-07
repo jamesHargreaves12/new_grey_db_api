982a6f9050f2f969c415286fcc3558b32d37c66d
jamesHargreaves12 <jamesHargreaves12@gmail.com>
2018-04-07 23:04:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var password = process.env.DBPASSWORD;
 var addr = process.env.MONGODB_ADDRESS;
 module.exports = {  
   db: {
-      production: "mongodb://" + username + ":" + password + "@" + addr + ":27017/new_grey_db_restapi_production",
+      production: "mongodb://" + username + ":" + password + "@" + addr + ":45218/new_grey_db_restapi_production",
       development: "mongodb://" + username + ":" + password + "@" + addr + ":27017/test",
       }
 };
